5f6abeeefba62b53f10bf59250a9d336218fd7df
AnshSingh0606 <ansh32511@gmail.com>
2026-02-09 13:46:47 +0530

    Add files via upload

---
+++ funt_sum.java
@@ -0,0 +1,23 @@
+package DSA_4sem;
+
+import java.util.Scanner;
+
+public class funt_sum {
+    static int add(int a, int b) {
+            return a + b;
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("first number: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("second number: ");
+            int num2 = sc.nextInt();
+
+            int result = add(num1, num2);
+            System.out.println("sum = " + result);
+        }
+    }
+
