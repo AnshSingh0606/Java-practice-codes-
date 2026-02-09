928daf9954f873f8a7fc0188f6a551c1ab7e005b
AnshSingh0606 <ansh32511@gmail.com>
2026-02-09 13:40:47 +0530

    Add files via upload

---
+++ fuct_fact.java
@@ -0,0 +1,20 @@
+package DSA_4sem;
+import java.util.Scanner;
+public class fuct_fact {
+    static int factorial(int n) {
+        int fact = 1;
+        for(int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int num = sc.nextInt();
+        System.out.println(factorial(num));
+    }
+}
+
